bf20d943ab39be1b27a579c270fc5ce33a4b469c
Jean Moscoso <jean.moscoso96@gmail.com>
2018-01-27 20:01:22 -0500

    Adding Pawn

---
+++ Pawn.java
@@ -0,0 +1,102 @@
+package chess.app;
+
+
+
+
+public class Pawn{
+	
+	char color;
+	int code = -1;
+	int posX = 0;
+	int posY = 0;
+	String str = "";
+	public Pawn(char color, int x, int y) {
+		this.color = color;
+		
+		if(color == 'b') {
+			 posX = x;
+			 posY =	y;	 
+			 code = 6;
+		}
+		
+		if(color == 'w') {
+			posX = 3;
+			posY = 7;
+			code = 0;
+		}
+		
+		
+		
+	}
+	
+	public int getCode() {
+		
+		return this.code;
+	}
+
+	
+	public int getPosX() {
+		
+		
+		return this.posX;
+	}
+	
+    public int getPosY() {
+		
+		
+		return this.posY;
+	}
+	
+	
+	
+	public boolean validMovement(int x, int y){
+
+		if(x == posX) {
+			posY = y;
+			return true;
+		} else if(y == posY) {
+			posX = x;
+			return true;
+		} else if(Math.abs(x - posX) == Math.abs(y - posY)) { 
+			
+			/*if(x > posX) {
+			if(y> posY) {
+				if((x - posX) == (y - posY)) {
+					return true;
+				}
+			} else if(y < posY) {
+				if(Math.abs(x - )())
+			}
+		} else if(y > posY) {
+			if(x>posX) {
+				
+			} else if(y>posY) {
+				
+			} */
+			posX = x;
+			posY = y;
+			return true;
+		}
+		
+		System.out.println("Move not allowed");
+		return false;
+	}
+	
+	
+	/*public String strCode() {
+		if(this.code == 0) {
+			str = "wQ";
+		} else if(this.code == 6) {
+			str = "bQ";
+		}
+		return str;
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+}
